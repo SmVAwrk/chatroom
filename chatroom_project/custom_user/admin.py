8fca290baf87350ac679bcea6fe52708cda405c9
SmVAwrk <smvawrk@gmail.com>
2021-03-09 18:59:36 +0300

    Добавление поля 'is_staff' в модель CustomUser и регистрация в админке

--- chatroom_project/custom_user/admin.py
+++ chatroom_project/custom_user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from custom_user.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
--- chatroom_project/custom_user/models.py
+++ chatroom_project/custom_user/models.py
@@ -22,13 +22,17 @@ class CustomUserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_staff', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
 
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
 
         return self._create_user(email, password, **extra_fields)
 
@@ -37,6 +41,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('e-mail address'), unique=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff status'), default=False)
 
     objects = CustomUserManager()
 
