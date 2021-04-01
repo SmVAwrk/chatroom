37354f3ac2875b8aae7416d80c901d4baeb89a6d
SmVAwrk <smvawrk@gmail.com>
2021-04-01 22:04:12 +0300

    Добавление swagger.json

--- chatroom_project/chatroom_project/settings.py
+++ chatroom_project/chatroom_project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 't7bsfx8xyn91)jl#s(%u#-#vn0jm8u==i2hko5z)1jpnb==q1r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'TRUE') == 'TRUE'
